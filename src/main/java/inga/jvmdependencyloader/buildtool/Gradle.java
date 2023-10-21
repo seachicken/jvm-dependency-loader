dcdb9748daecbb93cbf032f8515a9105aacfd85c
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-10-21 19:39:26 +0900

    Restore the original build.gradle

--- src/main/java/inga/jvmdependencyloader/buildtool/Gradle.java
+++ src/main/java/inga/jvmdependencyloader/buildtool/Gradle.java
@@ -9,17 +9,44 @@ import java.nio.file.StandardCopyOption;
 public class Gradle implements BuildTool {
     @Override
     public URLClassLoader load(Path root) {
+        backupBuildGradle(root);
         copyDependencies(root);
-        return new URLClassLoader(findJarUrls(root.resolve("target/dependency")));
+        var classLoader = new URLClassLoader(findJarUrls(root.resolve("target/dependency")));
+        restoreBuildGradle(root);
+        return classLoader;
     }
 
     private void copyDependencies(Path root) {
-        var gradleFile = Path.of(getClass().getClassLoader().getResource("ingaCopyDependencies.gradle").getFile());
+        var gradleStream = getClass().getClassLoader().getResourceAsStream("ingaCopyDependencies.gradle");
         try {
-            Files.copy(gradleFile, root.resolve("ingaCopyDependencies.gradle"), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(gradleStream, root.resolve("ingaCopyDependencies.gradle"), StandardCopyOption.REPLACE_EXISTING);
             var process = new ProcessBuilder("bash", "-c",
                     "echo apply \"from: 'ingaCopyDependencies.gradle'\" >> build.gradle && "
                             + "./gradlew ingaCopyDependencies -q")
+                    .directory(root.toFile())
+                    .start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void backupBuildGradle(Path root) {
+        try {
+            var process = new ProcessBuilder("bash", "-c",
+                    "cp build.gradle build.gradle.org")
+                    .directory(root.toFile())
+                    .start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void restoreBuildGradle(Path root) {
+        try {
+            var process = new ProcessBuilder("bash", "-c",
+                    "mv build.gradle.org build.gradle && rm ingaCopyDependencies.gradle")
                     .directory(root.toFile())
                     .start();
             process.waitFor();
